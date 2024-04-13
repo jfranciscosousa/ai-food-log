49441caac720c36b9bf32565e8ee8e8c0fcecbc8
Francisco Sousa <francisco.sousa@hey.com>
2024-04-13 12:47:24 +0100

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import { remixDevTools } from "remix-development-tools/vite";
+import { remixDevTools } from "remix-development-tools";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
