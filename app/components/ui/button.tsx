7d028c9fd0d5040f0fd7fa29442a61f15b3d4549
Francisco Sousa <francisco.sousa@hey.com>
2025-02-28 17:33:05 +0000

    Improve button loading

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -6,7 +6,7 @@ import { Loader2 } from "lucide-react";
 import { cn } from "~/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "relative inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -65,8 +65,13 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       >
         <span className="contents">
-          {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-          {children}
+          {isLoading && (
+            <Loader2 className="h-[70%] w-[70%] animate-spin absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+          )}
+
+          <span className={cn({ "text-transparent": isLoading })}>
+            {children}
+          </span>
         </span>
       </Comp>
     );
