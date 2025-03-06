e3c3a6e8a5ca68f091bb50335d276c58a5c9a846
Francisco Sousa <francisco.sousa@hey.com>
2025-03-06 15:51:18 +0000

    PWA

--- app/root.tsx
+++ app/root.tsx
@@ -113,6 +113,15 @@ function Document({
             name="viewport"
             content="width=device-width, initial-scale=1, maximum-scale=1"
           />
+          <link rel="icon" href="/favicon.ico" sizes="48x48" />
+          <link
+            rel="icon"
+            href="/favicon.svg"
+            sizes="any"
+            type="image/svg+xml"
+          />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/manifest.webmanifest" />
           {title ? <title>{title}</title> : null}
           <Meta />
           <Links />
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,52 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import tailwindcss from "@tailwindcss/vite";
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        theme_color: "#ffffff",
+        screenshots: [
+          {
+            src: "screenshot_1.png",
+            type: "image/png",
+            sizes: "1290x2796",
+            form_factor: "narrow",
+          },
+        ],
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }) as any,
+  ],
 });
